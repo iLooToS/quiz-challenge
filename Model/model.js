338557f579b32b4417b21e9d85a1d9cdca1e98b7
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-05-03 16:11:59 +0300

    fix: MVC not final

--- Model/model.js
+++ Model/model.js
@@ -1,10 +1,3 @@
-const readline = require('readline');
-const questionsAndAnswers = require('./QandA');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 const fs = require('fs');
 const { EOL } = require('os');
 const path = require('path');
@@ -33,33 +26,6 @@ class Model {
       }
     }
     return arrOfObj;
-  }
-
-  askQuestion() {
-    if (currentQuestionIndex < filteredQuestions.length) {
-      const currentQuestion = filteredQuestions[currentQuestionIndex].question;
-      console.log('=============================================');
-      console.log('Вопрос:\r\n', `${currentQuestion}\r\n`);
-
-      rl.question('Введите ответ: \r\n', (answer) => {
-        const correctAnswer = filteredQuestions[currentQuestionIndex].answer;
-
-        const correct = answer.trim().toLowerCase() === correctAnswer.toLowerCase();
-        printResultIcon(correct);
-
-        if (correct) {
-          correctAnswers++;
-        }
-
-        currentQuestionIndex++;
-        askQuestion();
-      });
-    } else {
-      const totalQuestions = filteredQuestions.length;
-      console.log(`Результат: ${correctAnswers}/${totalQuestions}`);
-
-      rl.close();
-    }
   }
 }
 module.exports = Model;
