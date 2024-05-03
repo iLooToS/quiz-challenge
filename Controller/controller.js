2654a39a099d0343232d675c76a76babe8a8bca0
Helga998 <bom1993@yandex.ru>
2024-05-03 16:49:42 +0300

    Final commit

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,11 +1,32 @@
+const Model = require("../Model/model");
+const View = require("../View/view");
+
 class Controller {
-  startQuiz(category) {
-    const filteredQuestions = questionsAndAnswers.filter(
-      (q) => q.category === category
+  static async runQuiz() {
+    let counter = 0;
+    const userCategory = await View.startQuiz();
+    console.log(`Вы выбрали категорию "${userCategory.category}"`);
+    let arrOfObjs;
+    console.log(userCategory.category, "CATEGORY");
+
+    if (userCategory.category === "Смешное") {
+      arrOfObjs = await Model.getQuestionsAndAnswers("funny.txt");
+    }
+    if (userCategory.category === "Еда") {
+      arrOfObjs = await Model.getQuestionsAndAnswers("eat.txt");
+    }
+    if (userCategory.category === "Эльбрус") {
+      arrOfObjs = await Model.getQuestionsAndAnswers("ElbrusFriends.txt");
+    }
+    for (const question of arrOfObjs) {
+      counter = await View.pullQuestions(question, counter);
+    }
+    console.log(
+      `Поздравляем, ты выиграл ${counter} очков в категории: ${userCategory.category}`
     );
-    const currentQuestionIndex = 0;
-    const correctAnswers = 0;
   }
 }
-
+// const result = await Model.getQuestionsAndAnswers('eat.txt');
+// console.log(result);
+Controller.runQuiz();
 module.exports = Controller;
